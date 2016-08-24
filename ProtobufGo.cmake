73dee055bc5aea79e980fac5e896b3a459d3b636
Lachlan Gunn <lachlan@twopif.net>
2016-08-24 09:42:12 +0200

    Transitioned to CMake, started switch to Protocol Buffers.

---
+++ ProtobufGo.cmake
@@ -0,0 +1,76 @@
+# Modified from FindProtobuf.cmake.
+#
+#=============================================================================
+# Copyright 2009 Kitware, Inc.
+# Copyright 2009-2011 Philip Lowman <philip@yhbt.com>
+# Copyright 2008 Esben Mose Hansen, Ange Optimization ApS
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+
+function(PROTOBUF_GENERATE_GO SRCS)
+  if(NOT ARGN)
+    message(SEND_ERROR "Error: PROTOBUF_GENERATE_GO() called without any proto files")
+    return()
+  endif()
+
+  SET(options)
+  SET(args_single OUTDIR)
+  SET(args_multi  SOURCES)
+  CMAKE_PARSE_ARGUMENTS(PBGG "${options}" "${args_single}" "${args_multi}" ${ARGN})
+
+  #if(PROTOBUF_GENERATE_CPP_APPEND_PATH)
+  #  # Create an include path for each file specified
+  #  foreach(FIL ${PBGG_SOURCES})
+  #    get_filename_component(ABS_FIL ${FIL} ABSOLUTE)
+  #    get_filename_component(ABS_PATH ${ABS_FIL} PATH)
+  #    list(FIND _protobuf_include_path ${ABS_PATH} _contains_already)
+  #    if(${_contains_already} EQUAL -1)
+  #        list(APPEND _protobuf_include_path -I ${ABS_PATH})
+  #    endif()
+  #  endforeach()
+  #else()
+  #  set(_protobuf_include_path -I ${CMAKE_CURRENT_SOURCE_DIR})
+  #endif()
+
+  if(DEFINED Protobuf_IMPORT_DIRS)
+    foreach(DIR ${Protobuf_IMPORT_DIRS})
+      get_filename_component(ABS_PATH ${DIR} ABSOLUTE)
+      list(FIND _protobuf_include_path ${ABS_PATH} _contains_already)
+      if(${_contains_already} EQUAL -1)
+          list(APPEND _protobuf_include_path -I ${ABS_PATH})
+      endif()
+    endforeach()
+  endif()
+
+  FILE(MAKE_DIRECTORY ${PBGG_OUTDIR})
+
+  set(${SRCS})
+  set(protos)
+  foreach(FIL ${PBGG_SOURCES})
+    get_filename_component(ABS_FIL ${FIL} ABSOLUTE)
+    get_filename_component(FIL_WE ${FIL} NAME_WE)
+    get_filename_component(ABS_DIR ${ABS_FIL} PATH)
+
+    list(APPEND ${SRCS} "${PBGG_OUTDIR}/${FIL_WE}.pb.go")
+    list(APPEND protos "${ABS_FIL}")
+  endforeach()
+
+    add_custom_command(
+      OUTPUT ${${SRCS}}
+      COMMAND  ${Protobuf_PROTOC_EXECUTABLE}
+      ARGS --go_out ${PBGG_OUTDIR} --proto_path ${CMAKE_CURRENT_SOURCE_DIR} ${_protobuf_include_path} ${protos}
+      DEPENDS ${ABS_FIL} ${Protobuf_PROTOC_EXECUTABLE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      COMMENT "Running Go protocol buffer compiler on ${FIL}"
+      VERBATIM )
+
+  set_source_files_properties(${${SRCS}} PROPERTIES GENERATED TRUE)
+  set(${SRCS} ${${SRCS}} PARENT_SCOPE)
+endfunction()
+
---
+++ src/test/test_randomset.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"requestor/randomset"
+)
+
+func order_to_number(order []int) int {
+	base := order[0]*2
+	if order[1] < order[2] {
+		return base+1
+	} else {
+		return base
+	}
+}
+
+func main() {
+	seed := make([]byte, 1)
+	seed[0] = 43
+
+	results := make([]int, 6)
+	i := 0
+	for ; i < 1000000; i++ {
+		identity := fmt.Sprintf("%d", i)
+		subset, err := randomset.RandomSubset(
+			seed, []byte(identity), 3, 3)
+		if err == nil {
+			index  := order_to_number(subset)
+			results[index]++
+		}
+	}
+
+	for j := 0; j < 6; j++ {
+		fmt.Printf("%d: %f\n", j,
+			(float64(results[j])-float64(i)/6.0)/(float64(i)))
+	}
+}
